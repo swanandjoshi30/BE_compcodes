9362039ed21f9411ef4522c8a433d6f65be87d63
Swanand Arvind Joshi <96830440+swanandjoshi30@users.noreply.github.com>
2025-11-04 23:53:31 +0530

    Refactor Fibonacci print statements and add complexities
    Updated print statements for consistency and added time/space complexity comments.

--- daa/fibo.py
+++ daa/fibo.py
@@ -3,19 +3,22 @@ def fibonacci_recursive(n):
         return n
     else:
         return fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2)
-
 num = int(input("Enter number: "))
-print("Fibonacci sequence (Recursive):")
+print("Fibonacci Sequence(Recursive)")
 for i in range(num + 1):
-    print(fibonacci_recursive(i), end=" ")
-    
-def fibonacci_iterative(n):
-    a, b = 0, 1
-    print("Fibonacci sequence (Iterative):")
-    print(a, end=" ")
-    for _ in range(n):
-        print(b, end=" ")
-        a, b = b, a + b
+    print(fibonacci_recursive(i),end=" ")
+# Time Complexity: O(2ⁿ)
+# Space Complexity:O(n)
 
+
+def fibonacci_iterative(n):
+    print("Fibonacci Sequence(Iterative):")
+    a, b = 0, 1
+    print(a,end=" ")
+    for _ in range(n):
+        print(b, end = " ")
+        a, b = b, a + b
 num = int(input("Enter number: "))
 fibonacci_iterative(num)
+# Time Complexity: O(n)
+# Space Complexity:O(1)
